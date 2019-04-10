68db879d44f69b22f82b50b91de90e859318a1e4
Jérémie Lumbroso <lumbroso@cs.princeton.edu>
2019-04-09 22:53:09 -0400

    Bugfix: Upload files with no extensions

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open("README.md").read()
 # This call to setup() does all the work
 setup(
     name="codePost-tools",
-    version="1.0.1",
+    version="1.0.2",
     description="Command line tools to manage codePost from the comfort of your terminal!",
     long_description=README,
     long_description_content_type="text/markdown",
